9699e36a2cccd5df9c68ec51b0c842ef7efd8ca9
Duvan Cardona Gutierrez <dcardonag@eafit.edu.co>
2020-04-17 09:02:55 -0500

    Se agrega detector de conflictos.

--- LogParser/LogParser.py
+++ LogParser/LogParser.py
@@ -18,27 +18,24 @@ class LogParser():
         self.categorize()
 
         eventCounter = 0
-
-        print('=============================================================================')
-        print('EVENTS FOUND')
-        print('=============================================================================')
+        conflictCounter = 0
 
         for key in self.bins:
 
             binContent = self.bins[key]
-            buffer = []
 
             for event in binContent['Events']: 
-                buffer.append(event.getEventStr())
+                print(event.getEventStr())
                 eventCounter += 1
-
-            if len(buffer) == 1: 
-                print(buffer[0])
-            else:
-                for k in buffer: print('[WARN]' + k)
+            
+            if len(binContent['Events']) > 1: 
+                conflictCounter += 1
+                for k in range(len(binContent['Events'])): print('*') 
 
         print(str(len(self.logs)) + ' log lines.')
         print(str(eventCounter) + " events found.")
+        print(str(conflictCounter) + " conflicts found.")
+
 
     def anonymize(self):
 
