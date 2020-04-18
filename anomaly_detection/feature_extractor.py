9beea961e94b310dee2f28e0648e20b5ddd017da
Duvan Cardona Gutierrez <dcardonag@eafit.edu.co>
2020-04-18 08:34:22 -0500

    Features extractor with new format.

--- anomaly_detection/feature_extractor.py
+++ anomaly_detection/feature_extractor.py
@@ -1,26 +1,31 @@
 from log_parser import LogParser
 
-
 class FeatureExtractor():
     def __init__(self, input_file):
         self.log_parser = LogParser(input_file)
         self.log_sequences = {}
 
     def extract(self):
-        events, logs = self.log_parser.parse() # events, blk_events
+        group_events_by_blk()
 
-        for log in logs:
-            blk = log['blk']
-            event_index = log['event']
+        for blk in self.log_sequences:
+            frequency_of_events = self.log_sequences[blk]
+
+            print('For block ' + blk + ' the event vector is ' + ', '.join(frequency_of_events))
+
+
+
+    def group_events_by_blk(self):
+        events, blk_events = self.log_parser.parse() # events, blk_events
+
+        num_events = len(events)
+
+        for blk_event in blk_events:
+            blk = blk_event[0]
+            event_index = blk_event[1]
 
             if blk not in self.log_sequences:
-                self.log_sequences[blk] = []
-
-                for e in events:
-                    self.log_sequences[blk].append(0)
-
+                self.log_sequences[blk] = [0] * num_events
                 self.log_sequences[blk][event_index] = 1
             else:
                 self.log_sequences[blk][event_index] += 1
-
-        return self.log_sequences
--- anomaly_detection/main.py
+++ anomaly_detection/main.py
@@ -1,8 +1,15 @@
 from log_parser import LogParser
+from feature_extractor import FeatureExtractor
+
+extractor = FeatureExtractor('HDFS_1K.log')
+
+log_sequences = extractor.extract()
+
+
 
 # lp1 = LogParser('HDFS_1K.log')
-lp1 = LogParser('HDFS_10.log')
+'''lp1 = LogParser('HDFS_10.log')
 lp1.anonymize()
 print(lp1.anonymized_data)
 lp1.tokenize()
-print(lp1.bins)
+print(lp1.bins)'''
