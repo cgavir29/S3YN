db6cbdf5630bc28a8da3d99c13a76ccfbc9ee94f
Duván Cardona Gutiérrez <dcardonageafit@gmail.com>
2020-05-15 16:37:39 -0500

    Se cambia la estructura de salida del Clustering.

--- backend/anomaly_detection/log_parser.py
+++ backend/anomaly_detection/log_parser.py
@@ -5,9 +5,10 @@ class LogParser():
         self.input_file = input_file
         self.anonymized_data = []
         self.log_sequences = {}
-        self.log_event = {}
-        self.events = []
         
+        self.tagged_events = {}
+        self.log_event = {}
+
         self.WORD_REGEX = r'([a-zA-Z]+)\s?'
         self.PARAM_REGEX = r'\<*\>'
 
@@ -16,7 +17,7 @@ class LogParser():
         self.extract_events()
         self.get_representative_log_sequence()
 
-        return self.events, self.log_sequences
+        return self.tagged_events, self.log_sequences
 
     def anonymize(self):
         '''
@@ -29,13 +30,19 @@ class LogParser():
             log_index = 0
             
             for log in log_file:
-                cleaned_log = log.split(':', 1)  # Remove unnecessary data
+                cleaned_log = log.split(':', 1) 
+                
+                type_event = cleaned_log[0].split()[3]
                 tokens = cleaned_log[1].split()
-                log_template = ''
+                
+                log_template = type_event + ':'
 
                 for token in tokens:
-                    if token[:5] == 'local' or token[:6] == 'remote':
+                    if token[:5] == 'local':
                         token = token.split('/')[0] + '<*>'
+                    
+                    elif token[:6] == 'remote':
+                        token = token.split('/')[0] + '<*>]'       
 
                     elif token[:3] == 'blk':
                         blk_id = token
@@ -51,22 +58,37 @@ class LogParser():
                     log_template += token + ' '
 
                 self.anonymized_data.append(log_template.strip())
-                log_index += 1
 
-    def extract_events(self):
-        for anonimized_log_index, anonimized_log in enumerate(self.anonymized_data):
-            if anonimized_log not in self.events: self.events.append(anonimized_log)
+                log_index += 1
             
-            self.log_event[anonimized_log_index] = self.events.index(anonimized_log)
+            del log_file
+
+    def extract_events(self):        
+        for anonimized_log_index, anonimized_log in enumerate(self.anonymized_data):
+            
+            typed_anonimized_log = anonimized_log.split(':', 1) 
+            
+            type_anonimized_log = typed_anonimized_log[0]
+            str_anonimized_log = typed_anonimized_log[1]
+
+            if str_anonimized_log not in self.tagged_events: 
+                self.tagged_events[str_anonimized_log] = type_anonimized_log
+
+            events = list(self.tagged_events.keys())
+
+            self.log_event[anonimized_log_index] = events.index(str_anonimized_log) 
+
+        del self.anonymized_data
 
     def get_representative_log_sequence(self):
         for key, value in self.log_sequences.items():
-            representative_log_sequence = [0] * len(self.events)
-            for anonymized_log_index in value['Logs']:
-                event_index = self.log_event[anonymized_log_index]
+            representative_log_sequence = [0] * len(self.tagged_events.keys())
+            
+            for log_index in value['Logs']:
+                event_index = self.log_event[log_index]
                 
-                if representative_log_sequence[event_index] == 0: representative_log_sequence[event_index] += 1
+                if representative_log_sequence[event_index] == 0: representative_log_sequence[event_index] = 1
 
             self.log_sequences[key]['Representative Log Sequence'] = representative_log_sequence
-
+            del self.log_sequences[key]['Logs']
    
--- backend/anomaly_detection/main.py
+++ backend/anomaly_detection/main.py
@@ -3,23 +3,24 @@ from feature_extractor import FeatureExtractor
 from clustering import Clustering
 from idf import IDF
 
-parser = LogParser('../../data/HDFS.log')
-events, log_sequences = parser.parse()
+parser = LogParser('../../data/HDFS_2K.log')
+tagged_events, log_sequences = parser.parse()
 
-for event in events:
-    print(event)
+extractor = FeatureExtractor(log_sequences, list(tagged_events.keys()))
+log_sequences = extractor.extract()
 
-#extractor = FeatureExtractor(log_sequences, events)
-#log_sequences = extractor.extract()
+events = list(tagged_events.keys())
+possible_abnormal_events = []
 
-# Se saca una lista con los posibles eventos anómalos.
-abnormal = [11]
+for key, value in tagged_events.items():
+    if value == 'WARN': possible_abnormal_events.append(key)
 
-#clustering = Clustering(log_sequences, events, abnormal)
-#log_sequences, clusters = clustering.cluster()
+print(possible_abnormal_events)
 
-'''for cluster_id in clusters:
-    print('Cluster ID: ' + str(cluster_id))
-    print('Num possible abnormal: ' + str(clusters[cluster_id]['Num Possible Abnormal']))
-    print(clusters[cluster_id]['Posibble Abnormal'])
-    print()'''
+clustering = Clustering(log_sequences, events, possible_abnormal_events)
+log_sequences, clusters = clustering.cluster()
+
+for cluster_value in list(clusters.values()):
+    if cluster_value['possible_abnormal_events'] != 0:
+        print(cluster_value)
+        print()
